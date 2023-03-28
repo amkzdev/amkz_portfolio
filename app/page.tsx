a5e5ffebc4de717b3b84e164205d32ca5d3de181
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-03-28 19:09:03 +0430

    Change Meta Tags

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,19 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 
+
+export const metadata = {
+  title: 'Amir Mahdi Keshan Zare',
+  description: 'This is Amir Mahdi Keshan Zare Offical Portfolio',
+}
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      Hi From Amkz Porfolio
-    </main>
+    < >
+      <button className='bg-yellow-500 p-2' >دکمه</button>
+    </>
   )
 }
