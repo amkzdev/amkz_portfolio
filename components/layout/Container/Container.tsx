e98bd2435f7996fbe10a7d8e8b36273192f788f5
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-03-28 21:02:42 +0430

    Add Theme Provider

--- components/layout/Container/Container.tsx
+++ components/layout/Container/Container.tsx
@@ -1,9 +1,17 @@
+ 'use client'
+import { firstTheme } from '@/theme/first'
+import { ThemeProvider, Container } from '@mui/material'
 import React, { ReactNode } from 'react'
 
-export const Container = ({ children }: { children: ReactNode }) => {
+const AppContainer = ({ children }: { children: ReactNode }) => {
     return (
-        <main className='container mx-auto px-4'>{
-            children
-        }</main>
+        <main className='container mx-auto px-4'>
+            <ThemeProvider theme={firstTheme}>
+                <Container maxWidth='xl'>
+                    {children}
+                </Container>
+            </ThemeProvider>
+        </main >
     )
 }
+export { AppContainer as Container } 
