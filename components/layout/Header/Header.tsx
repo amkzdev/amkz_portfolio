1c413c5c93949960f3facfe48cc95bee5106ad9f
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-03-28 21:02:58 +0430

    Modify Header

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,14 +1,13 @@
-'use client';
 import React from 'react'
-import { Box } from '@mui/material'
-import { Resposive } from './components/Resposive'
+// import { Box } from '@mui/material'
+// import { Resposive } from './components/Resposive'
 import { Desktop } from './components/Desktop'
 
 export const Header = () => {
     return (
-        <Box component={'header'}>
+        <header>
             <Desktop />
-            <Resposive />
-        </Box>
+            {/* <Resposive /> */}
+        </header>
     )
 }
