5ad8fa2e0d6d2324505ac520755fd271b39ec2de
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-03-28 20:51:14 +0430

    Add Mui Theme

---
+++ theme/first.ts
@@ -0,0 +1,75 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+declare module '@mui/material/styles' {
+  // interface Theme {
+  //   status: {
+  //     danger: React.CSSProperties['color'];
+  //   };
+  // }
+
+  interface Palette {
+    yellow: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    yellow: PaletteOptions['primary'];
+  }
+
+  interface PaletteColor {
+    // darker?: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    // darker?: string;
+  }
+
+  // interface ThemeOptions {
+  //   status: {
+  //     danger: React.CSSProperties['color'];
+  //   };
+  // }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    yellow: true;
+  }
+}
+
+declare module "@mui/material/Typography" {
+  interface ButtonPropsColorOverrides {
+    yellow: true;
+  }
+}
+
+
+declare module "@mui/material/Link" {
+  interface ButtonPropsColorOverrides {
+    yellow: true;
+  }
+}
+export const firstTheme = responsiveFontSizes(createTheme({
+  typography: {
+    fontFamily: 'Montserrat',
+    button:{
+      textTransform:'none'
+    }
+  },
+  palette: {
+    yellow: {
+      main: "#F6E71D",
+      contrastText: '#000',
+      light: '#FFFC72',
+      dark:'#e2e2e2',
+      
+
+    }
+  },
+  components:{
+    MuiButtonBase:{
+      styleOverrides:{
+        focusVisible:'rgba(0,0,0,0.1)'
+      }
+    }
+  }
+})) 
