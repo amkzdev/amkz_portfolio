7bd2599d74ef25d5dfb66f77f788c94d273d7a51
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-15 00:26:10 +0430

    Add Title To Links

--- components/Link.tsx
+++ components/Link.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const CustomLink = ({ text, fontWeight, href, ...props }: { href: string, text?: string, fontWeight?: string | number }) => {
     return (
-        <Link href={href} {...props} style={{ textDecoration: 'none' }} >
+        <Link href={href} {...props} style={{ textDecoration: 'none' }} title={`Go To AMKZ's ${text}`}>
             <Typography fontWeight={fontWeight || 500} color={'yellow.main'} sx={{ '&:hover': { color: 'primary.light' } }}>
                 {text}
             </Typography>
