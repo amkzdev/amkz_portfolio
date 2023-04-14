bab5e9ef0e9548753ade0088783619ef8fd5f640
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-15 00:23:38 +0430

    Change Seo And Links

--- components/layout/Header/components/Desktop.tsx
+++ components/layout/Header/components/Desktop.tsx
@@ -5,16 +5,17 @@ import lightLogo from 'assets/logos/yellow.png'
 import { Call } from '@mui/icons-material'
 import { Link } from '@components'
 import Image from 'next/image'
+import NextLink from 'next/link'
 
 export const Desktop = () => {
     const [mode, setMode] = useState<'sticky' | 'normal' | undefined>()
 
     const headerRef = useRef<HTMLInputElement>()
     const menuItems = [
-        { name: 'Projects', url: '/projects' },
-        { name: 'Work Experince', url: '#' },
-        { name: 'Skills', url: '#' },
-        { name: 'Education', url: '#' },
+        { name: 'Projects', url: '#projects' },
+        { name: 'Work Experince', url: '#workexperince' },
+        { name: 'Skills', url: '#skills' },
+        { name: 'Education', url: '#education' },
     ]
 
     useEffect(() => {
@@ -35,19 +36,21 @@ export const Desktop = () => {
     return (
         <>
             <Box sx={{ display: { xs: 'none', lg: 'flex' }, justifyContent: 'space-between', px: 2, py: 1.5, position: mode == 'sticky' ? 'fixed' : 'fixed', top: 0, width: '100%', maxWidth: '1488px', transition: 'all ease 0.4s', backgroundColor: '#020202', boxSizing: 'border-box', zIndex: 1000, boxShadow: mode == 'sticky' ? '0px  1px #F6E71D' : '' }} ref={headerRef}>
-                <Box sx={{ '& img': { width: '50px', height: '45px', mr: 1 }, display: 'flex', alignItems: "center", flex: 0.5 }}>
-                    <Image alt='amkz-light-logo' src={lightLogo} />
-                    <Typography fontWeight={'500'} color={'yellow.main'} fontSize={18}>Amir Mahdi Keshan Zare (AMKZ) </Typography>
-                </Box>
+                <NextLink href={'/'} title='Back to Home Page'>
+                    <Box sx={{ '& img': { width: '50px', height: '45px', mr: 1 }, display: 'flex', alignItems: "center"  , flexBasis:'20%'}}>
+                        <Image alt='Amir Mahdi Keshan Zare Logo' src={lightLogo} />
+                        <Typography fontWeight={'500'} color={'yellow.main'} fontSize={18} component={'h3'} title='Amir Mahdi Keshan Zare (AMKZ) Portfolio'> Amir Mahdi Keshan Zare (AMKZ)</Typography>
+                    </Box>
+                </NextLink>
 
-                <Box component={'nav'} sx={{ display: 'flex', flexDirection: "row", justifyContent: 'space-evenly', flex: 0.55, alignItems: 'center' }}>
+                <Box component={'nav'} sx={{ display: 'flex', flexDirection: "row", justifyContent: 'space-evenly', alignItems: 'center' }}>
                     {menuItems.map((item, index) => <>
-                        {!!index && <Typography color={'yellow.main'}>|</Typography>}
+                        {!!index && <Typography color={'yellow.main'} sx={{ mx: 2.5 }}>|</Typography>}
                         <Link text={item.name} href={item.url} />
                     </>)}
                 </Box>
 
-                <Box sx={{ display: 'flex', flexDirection: "row", justifyContent: 'flex-end', alignItems: 'center', flex: 0.5 }}>
+                <Box sx={{ display: 'flex', flexDirection: "row", justifyContent: 'flex-end', alignItems: 'center' , flexBasis:'20%' }}>
 
                     {/* <IconButton>
                     <LinkedIn color='primary' />
@@ -61,13 +64,13 @@ export const Desktop = () => {
                     <Typography fontWeight='bold'>Download</Typography>
                 </Button> */}
 
-                    <Button variant='contained' color='yellow' startIcon={<Call />} sx={{ ml: 2 }}>
+                    <Button variant='contained' color='yellow' title='Contact To Amir Mahdi Keshan Zare' startIcon={<Call />} sx={{ ml: 2 }}>
                         <Typography fontWeight='bold'>Contact</Typography>
                     </Button>
 
 
                 </Box>
-            </Box>
+            </Box >
 
             <Box height={'60px'} sx={{ transition: 'all ease 0.2s' }} />
         </>
