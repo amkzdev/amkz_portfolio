53eb1a69f00c54b091083d4093e1d3d6a25f61f1
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-15 17:15:34 +0430

    Add Fonts To Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", 
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Montserrat', 'ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Montserrat'],
+      'body': ['"Open Sans"'],
+    },
+    extend: {
+      colors: {
+        'amkz-yellow': '#F6E71D',
+      }
+
+    }
   },
   plugins: [],
 }
