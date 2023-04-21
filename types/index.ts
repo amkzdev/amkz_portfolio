c40b1955e142a92934f6ac408cf48475e52dade2
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-04-21 20:18:14 +0430

    Add Types

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface WorkExperinceType {
+    position: string | 'Frontend Developer',
+    companyName: string,
+    logo: any,
+    start_date: string,
+    end_date: string,
+    duration: string,
+    industry: string,
+    companyOverview: string[],
+    acheivements: string[]
+}
