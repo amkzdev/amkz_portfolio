d6c81867576d307aa83b698ec9cbeab0be2e8a1d
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-07-10 18:42:24 +0330

    Add Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 
-import { Container ,Header } from '@components/layout'
+import { Container, Header } from '@components/layout'
 import './globals.css'
+import { Montserrat } from './fonts'
 
 
 export default function RootLayout({
@@ -10,9 +11,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='bg-frame-black'>
-      <body className='bg-content-black m-0 p-0 container max-w-[1366px] mx-auto text-[12px] lg:text-[14px]'>
-          <Header />
-          {children}
+      <body className={'bg-content-black m-0 p-0 container max-w-[1366px] mx-auto text-[12px] lg:text-[14px] '.concat(Montserrat.className)}>
+        <Header />
+        {children}
       </body>
     </html>
   )
