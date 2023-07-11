1ff605106fcf1be7c062757cdfb72a138f2b9b33
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-07-11 23:00:40 +0330

    Create Section Header

---
+++ components/common/SectionHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SectionHeader = ({title}:{title:string}) => {
+  return (
+    <div className='flex flex-row items-center gap-5 w-full'>
+        <div className='bg-gray-500 flex-1 h-[1px]'></div>
+        <span className='text-gray-300 text-lg'>{title}</span>
+        <div className='bg-gray-500 flex-1 h-[1px]'></div>
+    </div>
+  )
+}
