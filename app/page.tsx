cad26d627b0f5fff7800f6e0c7c9d2682b0240d9
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-07-17 20:19:38 +0330

    Change Meta Data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
 
 import { Cover, Projects, TechnicalSkills } from '@components/homepage'
+import { Metadata } from 'next'
+import selfie from 'assets/logos/logoCircle.png'
 
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Amir Mahdi Keshan Zare (AMKZ) Portfolio',
-  description: 'This is Amir Mahdi Keshan Zare Offical Portfolio , I Am A Professional Front End Developer , Feel Free To Get In Touch With (amkz.dev@gmail.com)',
+  description: 'This is Amir Mahdi Keshan Zare Offical Portfolio , I Am A Professional Full Stack Developer , Feel Free To Get In Touch With (amkz.dev@gmail.com)',
+  authors: [{ name: 'Amir Mahdi Keshan Zare', url: 'amkz.dev@gmail.com' }],
+  // openGraph: {
+  //   type: 'profile',
+  //   description: 'This is Amir Mahdi Keshan Zare Offical Portfolio , I Am A Professional Full Stack Developer , Feel Free To Get In Touch With (amkz.dev@gmail.com)',
+  //   emails: ['amkz.dev@gmail.com'],
+  //   firstName: 'Amir Mahdi',
+  //   lastName: 'Keshan Zare',
+  //   title: 'Amir Mahdi Keshan Zare (AMKZ) Portfolio',
+  //   siteName: 'AMKZ Portfolio',
+  //   images: [
+  //     { url: selfie.src , type:'logo'}
+  //   ],
+    
+  // },
+  category:'Portfilo',
+  creator:'Amir Mahdi Keshan zare',
+  // metadataBase:{
+
+  // }
+  
 }
 
 
@@ -13,8 +35,8 @@ export default function Home() {
     <div className='px-6 flex flex-col gap-8 py-8'>
 
       <Cover />
-      <TechnicalSkills/>
-      <Projects/>
+      <TechnicalSkills />
+      <Projects />
     </div>
   )
 }
