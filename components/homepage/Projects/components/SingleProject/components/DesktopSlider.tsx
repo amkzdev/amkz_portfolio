bace906037d3739ec2fb8ecad012de7ae4b44f25
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-08-18 00:29:12 +0330

    Implement Slider

--- components/homepage/Projects/components/SingleProject/components/DesktopSlider.tsx
+++ components/homepage/Projects/components/SingleProject/components/DesktopSlider.tsx
@@ -2,36 +2,64 @@
 'use client'
 import { ProjectType } from '@/types';
 import Image from 'next/image';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperClass, SwiperRef, SwiperSlide } from 'swiper/react';
 import { Keyboard, Pagination, Navigation } from 'swiper/modules';
+import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
+import { Close } from '@mui/icons-material';
+// import Swiper from 'swiper';
 // Import Swiper styles
 
 export const DesktopSlider = ({ items, fullWidth }: { items: ProjectType['desktopShots'], fullWidth: boolean }) => {
-    return (
-        <Swiper
-            breakpoints={{
-                300: {
-                    spaceBetween: 10,
-                    slidesPerView: 1.5
-                },
-                1080: {
-                    spaceBetween: 10,
-                    slidesPerView: fullWidth ? 3.5 : 2.5
-                }
-            }}
-            style={{   paddingBottom:'40px' }}
-            className='h-full rounded bo rder border-gray-400 bg- gray-400 dark:bg-inherit '
-            modules={[Pagination]}
-            pagination={{enabled:true , clickable:true}}
-            // keyboard={{enabled:true , onlyInViewport:true }}
-        
 
-        >
-            {items?.map(item => <SwiperSlide key={item.img.src} className='bg-gray-200 rounded border border-gray-200 '>
-                <div className='w-full h-full '>
-                    <Image src={item.img} sizes='40vh' fill alt={item.description} className='rounded object-scale-down lg:object-contain' />
-                </div>
-            </SwiperSlide>)}
-        </Swiper>
+    const [open, setOpen] = useState<boolean>(false)
+
+    const [swiper, setSwiper] = useState<SwiperClass>()
+
+    const breakpoints = (state: boolean) => ({
+
+        300: {
+            spaceBetween: 10,
+            slidesPerView: 1.5
+        },
+        1080: {
+            spaceBetween: 10,
+            slidesPerView: fullWidth ? 3.5 : 2.5
+        }
+
+    })
+
+
+
+    useEffect(() => {
+        if (open)
+            swiper?.keyboard.enable()
+        else
+            swiper?.keyboard.disable()
+    }, [open])
+
+    return (
+
+        <div className={clsx(open ? 'fixed top-0 w-full h-full z-[500] r right-0 flex flex-col justify-center items-center backdrop-brightness-50 backdrop-blur-md p-[5%]' : 'w-full h-full')}  >
+
+            {open && <Close className='absolute left-8 top-8 text-gray-300 w-8 h-8 cursor-pointer hover:text-red-500' onClick={() => setOpen(false)} sx={{ width: 30, height: 30 }} />}
+
+            <Swiper
+                slidesPerGroup={2}
+                breakpoints={breakpoints(open)}
+                style={{ paddingBottom: '40px' }}
+                className={clsx('rounded  dark:bg-inherit cursor-pointer w-full', open ? 'aspect-square lg:aspect-video ' : ' lg: h-full ')}
+                modules={[Pagination, Keyboard]}
+                pagination={{ enabled: true, clickable: true }}
+                onClick={(e) => { setOpen(true) }}
+                onInit={(e) => setSwiper(e)}
+            >
+                {items?.map(item => <SwiperSlide key={item.img.src} className='bg-gray-200 rounded border border-gray-200 '>
+                    <div className='w-full h-full '>
+                        <Image src={item.img} sizes='100vh' loading='lazy' fill alt={item.description} className='rounded object-scale-down lg:object-contain' />
+                    </div>
+                </SwiperSlide>)}
+            </Swiper>
+        </div>
     );
 };
--- components/homepage/Projects/components/SingleProject/components/MobileSlider.tsx
+++ components/homepage/Projects/components/SingleProject/components/MobileSlider.tsx
@@ -1,28 +1,67 @@
 // Import Swiper React components
 'use client'
 import { ProjectType } from '@/types';
+import { Close } from '@mui/icons-material';
+import clsx from 'clsx';
 import Image from 'next/image';
-import { Pagination } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { useEffect, useState } from 'react';
+import ClickAwayListener from 'react-click-away-listener';
+import { Keyboard, Pagination } from 'swiper/modules';
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 
 export const MobileSlider = ({ items }: { items: ProjectType['desktopShots'] }) => {
+
+    const [open, setOpen] = useState<boolean>(false)
+
+    const [swiper, setSwiper] = useState<SwiperClass>()
+
+
+    const breakpoints = (state: boolean) => ({
+        0: {
+            slidesPerView: state ? 2.25 : 2.25
+        },
+        500: {
+            slidesPerView: state ? 3.2 : 2.75
+        },
+        1200: {
+            slidesPerView: state ? 3.5 : 4.5
+        }
+
+    })
+
+    useEffect(() => {
+        if (open)
+            swiper?.keyboard.enable()
+        else
+            swiper?.keyboard.disable()
+    }, [open])
+
     return (
-        <Swiper
-            dir='ltr'
-            spaceBetween={10}
-            slidesPerView={2.5}
-            style={{paddingBottom:'40px' }}
-            className='h-full rounded  dark:bg-inherit '
-            modules={[Pagination]}
-            pagination={{enabled:true , clickable:true}}
-        >
-            {items?.map(item => <SwiperSlide key={item.img.src} className='bg-gray-200  rounded '>
-                <div className='w-full h-fit'>
-                    <Image src={item.img.src} fill alt={item.description} fetchPriority='high' className='rounded object-contain' />
-                </div>
-            </SwiperSlide>)}
-        </Swiper>
+        <div className={clsx(open ? 'fixed top-0 w-full h-full z-[500] r right-0 flex flex-col justify-center items-center backdrop-brightness-50 backdrop-blur-md p-[5%]' : 'w-full h-full')}  >
+
+            {open && <Close className='absolute left-8 top-8 text-gray-300 w-8 h-8 cursor-pointer hover:text-red-500' onClick={() => setOpen(false)} sx={{ width: 30, height: 30 }} />}
+
+            <Swiper
+                dir='ltr'
+                spaceBetween={10}
+                breakpoints={breakpoints(open)}
+                // slidesPerView={open ? 3.5 : 2.5}
+                style={{ paddingBottom: '40px' }}
+                className={clsx('rounded  dark:bg-inherit cursor-pointer w-full', open ? 'aspect-square lg:aspect-video ' : ' lg: h-full ')}
+                modules={[Pagination , Keyboard]}
+                pagination={{ enabled: true, clickable: true }}
+                onClick={(e) => { setOpen(true) }}
+                onInit={(e) => setSwiper(e)}
+            >
+                {items?.map(item => <SwiperSlide onClick={() => setOpen(true)} key={item.img.src} className='bg-gray-100  rounded h- fit flex flex-col items-center justify-center h-3/4'>
+                    <div className='w-full h-fit   '>
+                        <Image src={item.img.src} fill sizes='100vh' alt={item.description} loading='lazy' className='rounded-xl overflow-hidden object-contain  aspect-video' />
+                    </div>
+                </SwiperSlide>)}
+            </Swiper>
+
+        </div>
     );
 };
